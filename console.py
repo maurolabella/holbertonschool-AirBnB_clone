ee29f3b6a2785f9bf995675b74cfa7de8b82e4c4
Alex <asenges400@gmail.com>
2022-06-29 16:42:56 -0300

    @asenges 2022-06-29 16:42:56

--- console.py
+++ console.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-import cmd, sys, os
+import cmd
+import sys
+import os
+import json
+import models
+import shlex
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
@@ -17,6 +23,33 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """Pass on empty line"""
+        pass
+
+    # ----- extended CLI commands ----- #
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, \
+        saves it (to the JSON file) and prints the id.\n"""
+        pass
+
+    def do_show(self, arg):
+        """Prints the string representation of \
+        an instance based on the class name and id.\n"""
+        pass
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class \
+        name and id (save the change into the JSON file).\n"""
+        pass
+
+    def do_all(self, arg):
+        """Prints all string representation of all \
+        instances based or not on the class name.\n"""
+        pass
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name \
+        and id by adding or updating attribute (save \
+        the change into the JSON file).\n"""
         pass
 
 
