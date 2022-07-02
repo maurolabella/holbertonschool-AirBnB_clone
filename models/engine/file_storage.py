f44a98497a587f0b76e6b0f3721eb0a40f452016
Alex <asenges400@gmail.com>
2022-07-02 17:27:16 -0300

    @asenges 2022-07-02 17:27:16

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,7 @@ import json
 class FileStorage():
     """serializes instances to a JSON file
     and deserializes JSON files to instances"""
-    __file_path = "./models/engine/file.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
